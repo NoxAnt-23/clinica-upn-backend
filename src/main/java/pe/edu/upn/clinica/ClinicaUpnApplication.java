950a3112831a365cc5ac3e7cf34f0d4af6f2499c
Marco Quispe <athon_2507@hotmail.com>
2026-06-01 00:27:03 -0500

    fix: desactivar seguridad automatica de spring security

--- src/main/java/pe/edu/upn/clinica/ClinicaUpnApplication.java
+++ src/main/java/pe/edu/upn/clinica/ClinicaUpnApplication.java
@@ -2,10 +2,13 @@ package pe.edu.upn.clinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration; //momentaneo
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) //Momentaneo
 public class ClinicaUpnApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ClinicaUpnApplication.class, args);
     }
+
 }
